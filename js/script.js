abe730e02cc26371de2abc7658b58db64af0df9f
Rossella <rossellataverni02@gmail.com>
2025-03-20 16:09:17 +0100

    [UPD] Update: let's break the first problem into subproblems.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Crea una lista di email di invitati ad una festa. 
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. 
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). 
+Si può fare? Certo che si basta ragionare un po’. 
+Nota: Non è necessario provvedere alla validazione delle email.*/
+
+//----------SCOMPONIAMO IL PROBLEMA----------
+// 1. Creiamo un array che contenga una lista di email
+// 2. Dichiariamo una variabile per l'inserimento dell'email dell'utente
+// 3. Controlliamo se l'email dell'utente è presente nell'array tramite un ciclo for
+// 4. Stampiamo messaggio appropriato sull'esito del controllo
